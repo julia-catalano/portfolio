2933beb5b4416a0714f854bd1cadc778c20ffcb8
Julia Catalano <jcatalano1990@gmail.com>
2020-02-19 14:44:33 -0500

    feat: css module header

--- src/components/header.js
+++ src/components/header.js
@@ -1,62 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import headerStyles from './header.module.css'
 
 const Header = () => (
-  <header
-    style={{
-      background: `blue`,
-      marginBottom: `1.45 rem`,
-    }}
-  >
-    <div
-      style={{
-        display: `flex`,
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <div>
-        <h6 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: `gray`,
-              textDecoration: `none`,
-            }}
-          >
-            {`home`}
-          </Link>
-        </h6>
-      </div>
-      <div>
-        <h6 style={{ margin: 0 }}>
-          <Link
-            to="/projects/"
-            style={{
-              alignSelf: `right`,
-              color: `gray`,
-              textDecoration: `none`,
-            }}
-          >
-            {`projects`}
-          </Link>
-        </h6>
-        <h6 style={{ margin: 0 }}>
-          <Link
-            to="/about/"
-            style={{
-              alignSelf: `right`,
-              color: `gray`,
-              textDecoration: `none`,
-            }}
-          >
-            {`about`}
-          </Link>
-        </h6>
-      </div>
-    </div>
+  <header className={headerStyles.header}>
+    <nav>
+      <ul className={headerStyles.navList}>
+        <li>
+          <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>home</Link>
+        </li>
+        <li>
+          <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/projects/'>projects</Link>
+        </li>
+        <li>
+          <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about/'>about</Link>
+        </li>
+      </ul>
+    </nav>
   </header>
 )
 
