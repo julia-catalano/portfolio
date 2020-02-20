465d0b3577c26066da8ab4cc4957b33529eaf07d
Julia Catalano <jcatalano1990@gmail.com>
2020-02-20 16:01:01 -0500

    update: styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,30 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import SmallLogo from '../images/logo-single.png'
 import headerStyles from './header.module.css'
 
 const Header = () => (
-  <header className={headerStyles.header}>
-    <nav>
-      <ul className={headerStyles.navList}>
-        <li>
-          <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>home</Link>
-        </li>
-        <li>
-          <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/projects/'>projects</Link>
-        </li>
-        <li>
-          <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about/'>about</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+    <nav className={headerStyles.headerContainer}>
+      <div className={headerStyles.navigationWrapper}>
+        <div>
+          <Link to='/'>
+            <img className={headerStyles.headerImage} src={SmallLogo}></img>
+          </Link>
+        </div>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/projects/'>projects</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about/'>about</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact/'>contact</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
 )
 
 Header.propTypes = {
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Contact" />
+    <h1>plz hire me</h1>
+    <p></p>
+  </Layout>
+)
+
+export default Contact
