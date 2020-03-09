8d5fba88f5133ecbe302ed83cf7ab63e4cf52534
Julia Catalano <jcatalano1990@gmail.com>
2020-03-09 17:18:22 -0400

    project cards

--- src/components/projectCardLeft.js
+++ src/components/projectCardLeft.js
@@ -4,7 +4,20 @@ import React from 'react'
 const ProjectCardLeft = (props) => {
   return (
     <div>
-      <h1>{props.title}</h1>
+      <img source={props.imageUrl} alt={props.imageAlt}/>
+      <div>
+        <h2>{props.title}</h2>
+        <h3>{props.date}</h3>
+        <p>{props.description}</p>
+        <p>stack: {props.technologies}</p>
+        <p>team: {props.team}</p>
+        <p>
+        <a href={props.github}>github</a>
+        </p>
+        <p>
+          { props.deployed ? <a href={props.deployed}></a> : null }
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,14 +12,13 @@ const Projects = () => (
     <h1>Hi from the projects space</h1>
     <div>
       {ProjectData.map((project,index) => {
-        if (index % 2 === 0) {
-          return <ProjectCardRight title={project.title}/>
-        } else {
-          return <ProjectCardLeft title={project.title}/>
+          if (index % 2 === 0) {
+            return <ProjectCardRight title={project.title} date ={project.date} imageUrl = {project.imageUrl} imageAlt ={project.imageAlt} description={project.description} technologies={project.technologies} team={project.team} github={project.github} deployed={project.deployed}/>
+          } else {
+            return <ProjectCardLeft title={project.title} date ={project.date} imageUrl = {project.imageUrl} imageAlt = {project.imageAlt} description={project.description} technologies={project.technologies} team={project.team} github={project.github} deployed={project.deployed}/>
+          }
         }
-      }
-        )}
-
+      )}
     </div>
   </Layout>
 )
