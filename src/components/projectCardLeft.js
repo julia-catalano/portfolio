644f9d8ab560d2216069528edb22579780f694ae
Julia Catalano <jcatalano1990@gmail.com>
2020-04-21 12:53:26 -0400

    fix: styling

--- src/components/projectCardLeft.js
+++ src/components/projectCardLeft.js
@@ -8,18 +8,17 @@ const ProjectCardLeft = (props) => {
   return (
     <div className="card-container">
       <div className='card-image-div'>
-      <img className="card-image" src={props.title === 'CINDR' ? CindrImg : TomatoImg } alt={props.imageAlt}/>
+      <img className="card-image" src={props.title === 'cindr' ? CindrImg : TomatoImg } alt={props.imageAlt}/>
       </div>
       <div className="card-content-wrapper-left">
         <p className="project-text-item-title">{props.title}</p>
         <p className="project-text-item-description">{props.description}</p>
         <div className="card-link">
-          <p className="project-text-item-link-container">
-            <img className="arrow-image" src={ArrowImg} alt={"an arrow pointing to the right"}/>
-          <a className="project-link" href={props.github}>github</a>
-          { props.deployed ? <a className="project-link" href={props.deployed}>visit</a> : null }
-          </p>
         <p className="project-text-item-stack">{props.technologies}</p>
+        <p className="project-text-item-link-container">
+          <a className="project-link" href={props.github}>GITHUB</a>
+          { props.deployed ? <a className="project-link" href={props.deployed}>VISIT</a> : null }
+          </p>
         <p className="project-text-item-team">{props.team}</p>
         </div>
       </div>
--- src/components/projectCardRight.js
+++ src/components/projectCardRight.js
@@ -10,18 +10,17 @@ const ProjectCardRight = (props) => {
       <div className="card-content-wrapper-right">
         <p className="project-text-item-title">{props.title}</p>
         <p className="project-text-item-description">{props.description}</p>
+        <p className="project-text-item-stack">{props.technologies}</p>
         <div className="card-link">
           <p className="project-text-item-link-container">
-          <img className="arrow-image" src={ArrowImg} alt={"an arrow pointing to the right"}/>
-          <a className="project-link" href={props.github}>github</a>
-          { props.deployed ? <a className="project-link" href={props.deployed}>visit</a> : null }
+          <a className="project-link" href={props.github}>GITHUB</a>
+          { props.deployed ? <a className="project-link" href={props.deployed}>VISIT</a> : null }
           </p>
         </div>
-        <p className="project-text-item-stack">{props.technologies}</p>
         <p className="project-text-item-team">{props.team}</p>
       </div>
       <div className='card-image-div'>
-        <img className="card-image" src={props.title === 'ALACK' ? AlackImg : StoopImg} alt={props.imageAlt}/>
+        <img className="card-image" src={props.title === 'alack' ? AlackImg : StoopImg} alt={props.imageAlt}/>
       </div>
     </div>
   )
